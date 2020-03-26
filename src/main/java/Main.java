6cf354d038a81ed83508a24d6176e446e2fbdd91
GrimBarber <achizhkoff@yahoo.com>
2020-03-26 00:03:30 -0700

    1st Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class Main {
+    public static void main(String[] args) {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("deviceName", "9af79710");
+        caps.setCapability("platformName", "android");
+        caps.setCapability("app", );
+    }
+}
